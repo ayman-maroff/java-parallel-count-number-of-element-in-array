b34e5a4f09d25540f6c115d31586c41e47a04e0f
ayman-maroff <aymnmrf@gmail.com>
2023-03-15 20:07:32 +0200

    4. find iterations parallel and sequentially stream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,26 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Stream;
 
 public class Main {
     //generate random array function
-    public static long[] createRandomArray(long n) {
+    public static Long[] createRandomArray(long n) {
         Random rd = new Random();
-        long[] array = new long[(int) n];
+        Long[] array = new Long[(int) n];
         int min = 1;
         int max = 100000;
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = rd.nextInt(max-min+1) + min;
+            array[i] = Long.valueOf(rd.nextInt(max-min+1) + min);
             System.out.print(array[i] +" ");
         }
         return array;
     }
-    public  static int CountIterationsSequentially(int number ,long[] array)
+    public  static int CountIterationsSequentially(int number ,Long[] array)
     {
         int count =0;
         for(int i = 0; i < array.length; i++)
@@ -29,19 +32,49 @@ public class Main {
         }
         return  count;
     }
+    public  static int CountIterationsSequentiallyStream(int number ,Long[] array)
+    {
 
+     Stream<Long> stream =  Arrays.stream(array);
+    var count =  stream.filter(num ->num ==number).count();
+        return (int) count;
+    }
+    public  static int CountIterationsParallelStream(int number ,Long[] array)
+    {
+
+        Stream<Long> stream =  Arrays.stream(array);
+        var count =  stream.parallel().filter(num ->num ==number).count();
+        return (int) count;
+    }
     public static void main(String[] args) {
 
-        long[] array = new long[10000000];
-        array = createRandomArray(10000000);
+        Long[] array = new Long[100000000];
+        array = createRandomArray(100000000);
         Scanner s = new Scanner(System.in);
         int n;
         System.out.print("\n Enter a number between 1 and 100000 to find the iterations of it:");
         n = s.nextInt();
         long start1 = System.currentTimeMillis();
-        System.out.print("the number of iterations is:"+CountIterationsSequentially(n,array) +"\n" );
+        System.out.print("the number of iterations(sequentially) is:"+CountIterationsSequentially(n,array) +"\n" );
         long end1 = System.currentTimeMillis();
         System.out.println("implementation Time in milli seconds: "+ (end1-start1));
 
+        ArrayCompute mainTask = new ArrayCompute(array, 0, 100000000,n);
+        ForkJoinPool pool = new ForkJoinPool();
+        long start2 = System.currentTimeMillis();
+        Integer NumberCount = pool.invoke(mainTask);
+        long end2 = System.currentTimeMillis();
+        System.out.println("the number of iterations (parallel) is: " + NumberCount);
+        System.out.println("implementation Time in milli seconds: "+ (end2-start2));
+
+        long start3 = System.currentTimeMillis();
+        System.out.print("the number of iterations(sequentially stream) is:"+CountIterationsSequentiallyStream(n,array) +"\n" );
+        long end3 = System.currentTimeMillis();
+        System.out.println("implementation Time in milli seconds: "+ (end3-start3));
+
+        long start4 = System.currentTimeMillis();
+        System.out.print("the number of iterations(parallel stream) is:"+CountIterationsParallelStream(n,array) +"\n" );
+        long end4 = System.currentTimeMillis();
+        System.out.println("implementation Time in milli seconds: "+ (end4-start4));
     }
 }
