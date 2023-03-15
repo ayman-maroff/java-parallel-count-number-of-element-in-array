34744bbc17da477665b7ae55b594b58f0ed2f3c2
ayman-maroff <aymnmrf@gmail.com>
2023-03-15 15:25:24 +0200

    2. find iterations sequentially

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     //generate random array function
@@ -16,10 +17,31 @@ public class Main {
         }
         return array;
     }
+    public  static int CountIterationsSequentially(int number ,long[] array)
+    {
+        int count =0;
+        for(int i = 0; i < array.length; i++)
+        {
+            if(array[i] == number)
+            {
+                count++;
+            }
+        }
+        return  count;
+    }
 
     public static void main(String[] args) {
 
         long[] array = new long[10000000];
         array = createRandomArray(10000000);
+        Scanner s = new Scanner(System.in);
+        int n;
+        System.out.print("\n Enter a number between 1 and 100000 to find the iterations of it:");
+        n = s.nextInt();
+        long start1 = System.currentTimeMillis();
+        System.out.print("the number of iterations is:"+CountIterationsSequentially(n,array) +"\n" );
+        long end1 = System.currentTimeMillis();
+        System.out.println("implementation Time in milli seconds: "+ (end1-start1));
+
     }
 }
